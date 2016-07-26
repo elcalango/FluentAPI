dcfa6775e9d992031cd987e350f61db8703b5a37
elcalando <reato.neto@gmail.com>
2016-07-25 21:58:15 -0300

    Add project files.

---
+++ FluentAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FluentAPI.Startup))]
+namespace FluentAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
